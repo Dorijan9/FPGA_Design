5cc9d865066ddf927fc8dedec395bb3741ff2659
Dorian Donaj Magasic <88779842+Dorijan9@users.noreply.github.com>
2025-04-21 19:24:32 +0200

    Update level2.v

--- level2.v
+++ level2.v
@@ -11,63 +11,53 @@ module level2(
 );
     // Maze config constants
     assign TILE_W = 10'd288;
-    assign TILE_H = 10'd160; // Adjusted to fill height
+    assign TILE_H = 10'd160; 
     assign NUM_ROWS = 3'd5;
-    assign NUM_COLS = 3'd7;
+    assign NUM_COLS = 3'd5;
     assign WALL_MARGIN = 10'd3;
 
-    reg [3:0] maze [0:4][0:6];  // FIXED: range [0:6] to match NUM_COLS=7
+    reg [3:0] maze [0:4][0:4]; 
 
     initial begin
     // Row 0
-    maze[0][0] = 4'b1110;
-    maze[0][1] = 4'b1100;
-    maze[0][2] = 4'b1000; 
-    maze[0][3] = 4'b1110;
+    maze[0][0] = 4'b1011;
+    maze[0][1] = 4'b1010;
+    maze[0][2] = 4'b1000;
+    maze[0][3] = 4'b1010;
     maze[0][4] = 4'b1001;
-    maze[0][5] = 4'b1001; 
-    maze[0][6] = 4'b1001; 
-
-        // Row 1
-        maze[1][0] = 4'b1010;
-        maze[1][1] = 4'b1001;
-        maze[1][2] = 4'b0110;
-        maze[1][3] = 4'b1100;
-        maze[1][4] = 4'b0001;
-        maze[1][5] = 4'b1111;
-        maze[1][6] = 4'b1111;
-
-        // Row 2
-        maze[2][0] = 4'b0111;
-        maze[2][1] = 4'b0000;
-        maze[2][2] = 4'b0000;
-        maze[2][3] = 4'b0000;
-        maze[2][4] = 4'b0000;
-        maze[2][5] = 4'b1101;
-        maze[2][6] = 4'b1111;
-
-        // Row 3
-        maze[3][0] = 4'b0010;
-        maze[3][1] = 4'b0101;
-        maze[3][2] = 4'b1100;
-        maze[3][3] = 4'b0101;
-        maze[3][4] = 4'b0001;
-        maze[3][5] = 4'b1111;
-        maze[3][6] = 4'b1111;
-
-        // Row 4
-        maze[4][0] = 4'b0110;
-        maze[4][1] = 4'b0100;
-        maze[4][2] = 4'b0001;
-        maze[4][3] = 4'b0100;
-        maze[4][4] = 4'b0101;
-        maze[4][5] = 4'b1111;
-        maze[4][6] = 4'b1111;
+    
+    // Row 1
+    maze[1][0] = 4'b0000;
+    maze[1][1] = 4'b0010;  
+    maze[1][2] = 4'b0000;
+    maze[1][3] = 4'b0000;
+    maze[1][4] = 4'b0001;
+    
+    // Row 2
+    maze[2][0] = 4'b0010;
+    maze[2][1] = 4'b1000;
+    maze[2][2] = 4'b0110;
+    maze[2][3] = 4'b0111;
+    maze[2][4] = 4'b0001;
+    
+    // Row 3
+    maze[3][0] = 4'b0010;
+    maze[3][1] = 4'b1001;
+    maze[3][2] = 4'b1010;
+    maze[3][3] = 4'b0000;
+    maze[3][4] = 4'b0011;  
+    
+    // Row 4
+    maze[4][0] = 4'b0110;
+    maze[4][1] = 4'b0100;
+    maze[4][2] = 4'b0000; 
+    maze[4][3] = 4'b0110;  
+    maze[4][4] = 4'b0101;
     end
 
-    // ACTUAL WALL LOOKUP: This was missing in your original module.
+   
     always @(*) begin
-        if (row < 5 && col < 7)
+        if (row < 5 && col < 5)
             walls = maze[row][col];
         else
             walls = 4'b0000;  // Default no walls if out of bounds
