635f8dfe4949fe13178cef0ff232462a9eb63cb2
Dorian Donaj Magasic <88779842+Dorijan9@users.noreply.github.com>
2025-04-21 20:54:39 +0200

    Update score_counter.v

--- score_counter.v
+++ score_counter.v
@@ -1,14 +1,16 @@
+`timescale 1ns / 1ps
 module score_counter (
     input clk,
     input rst,
     input level_complete,
+    input heart_collected,
     output reg [7:0] score
 );
 
     always @(posedge clk or posedge rst) begin
         if (rst)
             score <= 8'd0;
-        else if (level_complete)
+        else if (level_complete || heart_collected)
             score <= score + 8'd1;
     end
 
