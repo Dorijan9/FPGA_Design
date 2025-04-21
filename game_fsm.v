ad6dadc8e614836de394205c800563462761144d
Dorian Donaj Magasic <88779842+Dorijan9@users.noreply.github.com>
2025-04-21 20:13:46 +0200

    Update game_fsm.v

--- game_fsm.v
+++ game_fsm.v
@@ -1,28 +1,41 @@
+`timescale 1ns / 1ps
 module game_fsm(
     input clk,
     input rst,
     input [4:0] player_row,
     input [4:0] player_col,
-    output reg [1:0] level_select
+    output reg [1:0] level_select,
+    output reg win
 );
 
     // State constants
     wire [1:0] LEVEL1 = 2'd0;
     wire [1:0] LEVEL2 = 2'd1;
     wire [1:0] LEVEL3 = 2'd2;
+    wire [1:0] WIN_STATE = 2'd3;
 
     always @(posedge clk or negedge rst) begin
-        if (!rst)
+        if (!rst) begin
             level_select <= LEVEL1;
-        else begin
+            win <= 0;
+        end else begin
             case (level_select)
-                2'd0: if (player_row == 5'd4 && player_col == 5'd2)
-                          level_select <= LEVEL2;
+                LEVEL1:
+                    if (player_row == 5'd4 && player_col == 5'd2)
+                        level_select <= LEVEL2;
 
-                2'd1: if (player_row == 5'd5 && player_col == 5'd2)
-                          level_select <= LEVEL3;
+                LEVEL2:
+                    if (player_row == 5'd5 && player_col == 5'd2)
+                        level_select <= LEVEL3;
 
-                default: level_select <= level_select;
+                LEVEL3:
+                    if (player_row == 5'd0 && player_col == 5'd4) begin
+                        level_select <= WIN_STATE;
+                        win <= 1;
+                    end
+
+                WIN_STATE:
+                    level_select <= WIN_STATE; // Stay locked in
             endcase
         end
     end
