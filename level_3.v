ffc708ef7d952af5dcfbcc5294fbece8778f27a2
Dorian Donaj Magasic <88779842+Dorijan9@users.noreply.github.com>
2025-04-21 19:22:54 +0200

    Update level_3.v

--- level_3.v
+++ level_3.v
@@ -15,7 +15,7 @@ module level3(
     assign TILE_W = 10'd288;
     assign TILE_H = 10'd160;
     assign NUM_ROWS = 5'd5;
-    assign NUM_COLS = 5'd10;
+    assign NUM_COLS = 5'd5;
     assign WALL_MARGIN = 10'd3;
 initial begin
 // Row 0
@@ -23,59 +23,39 @@ initial begin
     maze[0][1] = 4'b1100;
     maze[0][2] = 4'b1001;
     maze[0][3] = 4'b1010;
-    maze[0][4] = 4'b1100;
-    maze[0][5] = 4'b1000;
-    maze[0][6] = 4'b1100;
-    maze[0][7] = 4'b1000;
-    maze[0][8] = 4'b1100;
-    maze[0][9] = 4'b1111;
+    maze[0][4] = 4'b1101;
+ 
     // Row 1
     maze[1][0] = 4'b1010;
     maze[1][1] = 4'b1100;
     maze[1][2] = 4'b0101;
     maze[1][3] = 4'b0010;
     maze[1][4] = 4'b1101;
-    maze[1][5] = 4'b0110;
-    maze[1][6] = 4'b1001;
-    maze[1][7] = 4'b0111;
-    maze[1][8] = 4'b1010;
-    maze[1][9] = 4'b1001;
+
     // Row 2
     maze[2][0] = 4'b0110;
     maze[2][1] = 4'b1001;
     maze[2][2] = 4'b1110;
     maze[2][3] = 4'b0001;
-    maze[2][4] = 4'b1010;
-    maze[2][5] = 4'b1001;
-    maze[2][6] = 4'b0110;
-    maze[2][7] = 4'b1100;
-    maze[2][8] = 4'b0101;
-    maze[2][9] = 4'b0011;
+    maze[2][4] = 4'b1011;
+
     // Row 3
     maze[3][0] = 4'b1011;
     maze[3][1] = 4'b0110;
     maze[3][2] = 4'b1000;
     maze[3][3] = 4'b0101;
     maze[3][4] = 4'b0011;
-    maze[3][5] = 4'b0110;
-    maze[3][6] = 4'b1100;
-    maze[3][7] = 4'b1001;
-    maze[3][8] = 4'b1010;
-    maze[3][9] = 4'b0001;
+
     // Row 4
     maze[4][0] = 4'b0110;
     maze[4][1] = 4'b1100;
     maze[4][2] = 4'b0100;
     maze[4][3] = 4'b1100;
     maze[4][4] = 4'b0101;
-    maze[4][5] = 4'b1110;
-    maze[4][6] = 4'b1100;
-    maze[4][7] = 4'b0100;
-    maze[4][8] = 4'b0101;
-    maze[4][9] = 4'b0110;
+
 end
 always @(*) begin
-        if (row < 5 && col < 10)
+        if (row < 5 && col < 5)
             walls = maze[row][col];
         else
             walls = 4'b0000; // Default if out of bounds
